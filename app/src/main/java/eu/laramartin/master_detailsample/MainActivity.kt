8a4e0189742efff3ab023156338a7ee1103284f8
tynn <tynn.dev@gmail.com>
2020-05-02 17:04:57 +0200

    Combine click listeners with child fragment manager

--- app/src/main/java/eu/laramartin/master_detailsample/MainActivity.kt
+++ app/src/main/java/eu/laramartin/master_detailsample/MainActivity.kt
@@ -3,42 +3,26 @@ package eu.laramartin.master_detailsample
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
-    private lateinit var navHostFragment: NavHostFragment
+    private inline val nav get() = findNavController(R.id.main_container)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        navHostFragment = supportFragmentManager.findFragmentById(R.id.main_container) as NavHostFragment
-
-        findViewById<BottomNavigationView>(R.id.bottom_navigation_view)
-            .setupWithNavController(navController = navHostFragment.navController)
-
-        navHostFragment.navController.addOnDestinationChangedListener { _, destination, _ ->
-            supportActionBar!!.title = destination.label
-            when (destination.id) {
-                R.id.fragment_account,
-                R.id.fragment_settings,
-                R.id.fragment_notifications -> {
-                    supportActionBar!!.setDisplayHomeAsUpEnabled(true)
-                }
-                else -> supportActionBar!!.setDisplayHomeAsUpEnabled(false)
-            }
-        }
+        val nav = nav
+        setupActionBarWithNavController(nav)
+        bottom_navigation_view.setupWithNavController(nav)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            android.R.id.home -> {
-                navHostFragment.navController.popBackStack()
-                return true
-            }
-        }
-        return super.onOptionsItemSelected(item)
+        return if (item.itemId == android.R.id.home) {
+            nav.popBackStack()
+            true
+        } else super.onOptionsItemSelected(item)
     }
 }
--- app/src/main/java/eu/laramartin/master_detailsample/ProfileFragment.kt
+++ app/src/main/java/eu/laramartin/master_detailsample/ProfileFragment.kt
@@ -1,64 +1,33 @@
 package eu.laramartin.master_detailsample
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import android.widget.TextView
-import androidx.navigation.Navigation
-import androidx.navigation.fragment.NavHostFragment
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_profile.*
 
-class ProfileFragment : Fragment() {
+class ProfileFragment : Fragment(R.layout.fragment_profile) {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        lateinit var view: View
+    private inline val navContext
+        get() = childFragmentManager
+            .findFragmentById(R.id.profile_nav_container)
+            ?: this
 
-        val isTablet = requireContext().resources.getBoolean(R.bool.isTablet)
+    private val nav: NavController
+        get() = navContext.findNavController()
 
-        when {
-            isTablet -> {
-                view = inflater.inflate(R.layout.fragment_profile_land, container, false)
-                displayMasterDetailLayout(view)
-            }
-            else -> {
-                view = inflater.inflate(R.layout.fragment_profile, container, false)
-                displaySingleLayout(view)
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        account_textview.setOnClickListener {
+            nav.navigate(R.id.fragment_account)
         }
 
-        return view
-    }
-
-    private fun displaySingleLayout(view: View) {
-        view.findViewById<TextView>(R.id.account_textview).setOnClickListener(
-            Navigation.createNavigateOnClickListener(R.id.action_profile_fragment_to_fragment_account)
-        )
-        view.findViewById<TextView>(R.id.notifications_textview).setOnClickListener(
-            Navigation.createNavigateOnClickListener(R.id.action_profile_fragment_to_fragment_notifications)
-        )
-        view.findViewById<TextView>(R.id.settings_textview).setOnClickListener(
-            Navigation.createNavigateOnClickListener(R.id.action_profile_fragment_to_fragment_settings)
-        )
-    }
-
-    private fun displayMasterDetailLayout(view: View) {
-        val navHostFragment =
-            childFragmentManager.findFragmentById(R.id.profile_nav_container) as NavHostFragment
-
-        view.findViewById<TextView>(R.id.account_textview).setOnClickListener {
-            navHostFragment.navController.navigate(R.id.fragment_account)
+        notifications_textview.setOnClickListener {
+            nav.navigate(R.id.fragment_notifications)
         }
 
-        view.findViewById<TextView>(R.id.notifications_textview).setOnClickListener {
-            navHostFragment.navController.navigate(R.id.fragment_notifications)
-        }
-
-        view.findViewById<TextView>(R.id.settings_textview).setOnClickListener {
-            navHostFragment.navController.navigate(R.id.fragment_settings)
+        settings_textview.setOnClickListener {
+            nav.navigate(R.id.fragment_settings)
         }
     }
 }
